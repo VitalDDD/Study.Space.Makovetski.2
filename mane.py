8a0c5d97eadf4d73bc03df0901f1d17536fd3c9c
unknown <dmitry.makovetski@gmail.com>
2023-01-01 00:20:46 +0200

    fourth commit

--- mane.py
+++ mane.py
@@ -9,3 +9,14 @@ def mane_function(slov, nazva):
     print("Good luck!")
     return sl
 
+def pr_nt(p):
+    i,j=0,0
+    for f in os.listdir(p):
+        if os.path.isdir(f):
+            j+=1
+            print("|", f)
+        else:
+            i+= 1
+            print(f)
+    print(f"Каталогів: {j}", f"Файлів: {i}")
+
